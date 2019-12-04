c5e316e6628d0c902c1650773fb15a3d45150f3e
Gerardo Ferreyra <gerardof22@gmail.com>
2019-12-04 16:33:25 -0300

    Creación de reporte para Cobranza

--- Gimnasio/FrmCobranza.cs
+++ Gimnasio/FrmCobranza.cs
@@ -210,8 +210,8 @@ namespace Gimnasio
                 throw;
             }
 
-            //Reportes.Cobranzas cobranzas = new Reportes.Cobranzas(this.cobranza.idcobranza);
-            //cobranzas.ShowDialog();
+            Reportes.ReporteCobranza cobranzas = new Reportes.ReporteCobranza(this.cobranza.idcobranza);
+            cobranzas.ShowDialog();
         }
 
         private void chekDebe_CheckedChanged(object sender, EventArgs e)
---
+++ Gimnasio/Reportes/ReporteCobranza.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Datos;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gimnasio.Reportes
+{
+    public partial class ReporteCobranza : Form
+    {
+        GimnasioContext dbGimnasio = new GimnasioContext();
+        private int idcobranza;
+
+        public ReporteCobranza(int idenviado)
+        {
+            InitializeComponent();
+            this.idcobranza = idenviado;
+        }
+
+        private void ReporteCobranza_Load(object sender, EventArgs e)
+        {
+            var listaCobranza = from cobranza in dbGimnasio.Detalle_Cobranzas
+                                join detalle in dbGimnasio.Detalle_Cobranzas on cobranza.idcobranza equals detalle.Cobranza.idcobranza
+                                where cobranza.idcobranza == this.idcobranza
+                                select new
+                                {
+                                    idcobranza = cobranza.idcobranza,
+                                    fechaPago = cobranza.Cobranza.fechaPago,
+                                    recargoMes = detalle.recargoMes,
+                                    importe = detalle.importe,
+                                    detalleCobranza_total = detalle.detalleCobranza_total,
+                                    cobranza_total = cobranza.Cobranza.cobranza_total
+                                };
+            CobranzaBindingSource.DataSource = listaCobranza.ToList();
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
